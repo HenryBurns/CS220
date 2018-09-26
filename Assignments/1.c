8524958ac620835416a51258b405db8d24743140
Henry Burns <hburns3@cs.binghamton.edu>
2018-09-26 01:17:31 -0400

    fixed assignment 1 q2&3

--- Assignments/1.c
+++ Assignments/1.c
@@ -11,62 +11,99 @@ int **custom_allocator(unsigned int n){
 	int** arr = (int**) (malloc(sizeof(int*) * 2*n-1));
 	int i;
 	int cntr = n-1;
-	for(i = n; i < 2*n-1 ; i++){
 	for(i = 0; i < n; i++){
 		arr[i] = (int*) (malloc(sizeof(int) * (i + 1)));
 	}
-	int cntr = n-1;
 	for(i = n; i < 2*n-1 ; i++){
 		arr[i] = (int*) (malloc(sizeof(int) * (cntr)));
 		cntr--;
 	}
+	return arr;
 }
-
+void printTransitionVector(unsigned char ansr){
+	int j;
+	for(j = 0; j < 8; j++)
+		printf("%d", !!((ansr << j)& 0x80));
+	printf("\n Transition Vector test is over");
+}
 unsigned char transition_vector(unsigned char v){
 	unsigned char ansr = (unsigned char) 0;
 	ansr |= (((BIT(2) & v) << 1) ^ (BIT(3)&v)) <<4;
 	ansr |= (((BIT(0) & v) << 6));
 	ansr |= (((BIT(1) & v) << 1) | (BIT(2) & v)) << 5;
-	ansr |= (((BIT(4) & v) & ((BIT(7) & v)>>3) & (~((BIT(0)&v)<<4)))& BIT(4)); 
+	ansr |= ((BIT(4) & v) & ((BIT(7) & v)>>3) & (~((BIT(0)&v)<<4))& BIT(4)); 
 	ansr |= ((~((BIT(5) & v)>>2)) & BIT(5));
-	unsigned char temp = (unsigned char) (0);
-	for(int i = 0; i <8; i++){
+	unsigned char temp = (((BIT(0) & v) >> 0));
+	for(int i = 1; i <8; i++){
 		temp = temp ^ (((BIT(i) & v) >> i));
 	}
 	temp = temp << 2;
 	ansr |= temp;
 	ansr |= ((BIT(1) & v ) & ((BIT(2) & v) >> 1));
 	ansr |= (((BIT(5) & v)>>5) | ((BIT(6) & v) >>6)); 
+	return ansr;
 }
 
 void paritize (void *p, unsigned int num_bytes){
 	int cntr = 0;
+	unsigned char temp;
+	unsigned char v;
+	int i;
 	while(num_bytes > 0){
-		unsigned char v = *(((unsigned char*) p )+ (num_bytes-1));
-		unsigned char temp = v>>1;
-		while(temp != (unsigned char) 0){
+		v = *(((unsigned char*) p )+ (num_bytes-1));
+		//printf("initial value: %d", v);
+		temp = v;
+		temp = temp >>1;
+		for(int i = 0; i < 7; i++){
+			if(temp % 2 == 1)
+				cntr++;
+		//	printf("temp: %d ", temp);
 			temp = temp >>1;
-			cntr++;
 		}
-		v = (cntr%2 == 2)? v | 1: v & -2;
+		//printf("cntr: %d \n", cntr);
+		*(((unsigned char*) p )+ (num_bytes-1))= (cntr%2 == 1)? v | 1: v & -2;
+		//printf(" Value: %d\n", (int)v);
 		num_bytes--;
 		cntr = 0;
 	}
 }
+void testing_paratize(int num){
+	int* pptr = &num;
+	printf("intial number: %d", num);
+	paritize((void*) pptr, sizeof(num));
+	printf("\n ansr: %d\n\n\n", num);
+}
 int main(){
+	//TESTING Custom Allocator
+	printf("Starting code\n");
 	int** arr = custom_allocator(2);
+	printf("TEST\n");
 	int i = 0;
 	for(i = 0; i < 3; i++){
 		arr[i][0] = 1;
 	}
+	arr[1][1] = 3;
 	int j = 0;
+	int rows = 1;
 	for(i = 0; i < 3; i++){
-		while(arr[i][j] != NULL){
-			printf("%d ", arr[i][j]);
-			j++;
+			if( i == 1)
+				rows = 2;
+			else
+				rows = 1;
+			for(int j = 0; j < rows; j++)
+				printf("%d ", arr[i][j]);
+			printf("\n");
 		}
-		printf("\n");
-		j = 0;
+	//TESTING Transition Vector
+	unsigned char ansr = 0x65;
+	ansr = transition_vector(ansr);
+	for(j = 0; j < 8; j++)
+		printf("%d", !!((ansr << j)& 0x80));
+	printf("\n");
+	//TESTING paratize
+	int p = 2;
+	for(p = 0; p < 10; p++){
+		testing_paratize(p);
 	}
-	
 }
+
